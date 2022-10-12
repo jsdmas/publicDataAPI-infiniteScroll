5fab881120e5f4a3126a4beb177594f2ad7b048c
reina7532 <reinaprc@gmail.com>
2022-10-12 17:11:56 +0900

    end

--- js/elementCreate.js
+++ js/elementCreate.js
@@ -73,44 +73,58 @@ function elementcreate(v) {
         if (Existence === true) {
             description.classList.toggle("hidden");
         };
-            // 이미지 변화
-            poster.setAttribute("src", v.IMGURL);
-            // title
-            infoTitle.innerHTML = v.SVCNM;
-    
-            // SVCOPNBGNDT : 서비스 시작일시
-            // SVCOPNENDDT : 서비스 종료일시
-            // span1 : 날짜 -> string 변환후 substring으로 날짜만 표시.
-            span1.innerHTML = `${String(v.SVCOPNBGNDT).substring(0, 10)} ~ ${String(v.SVCOPNENDDT).substring(0, 10)}`;
-    
-            // span2 : 결재방법
-            span2.innerHTML = v.PAYATNM;
-    
-            // span3 : 서비스 상태
-            span3.innerHTML = v.SVCSTATNM;
-    
-            // span4 : 지역명
-            span4.innerHTML = v.AREANM;
-    
-    
-            // moreInfo : 상세내용
-            const DTLCONTNum = v.DTLCONT.indexOf("3.");
-            const deleteDT = v.DTLCONT.slice(0, DTLCONTNum);
-            const text = v.DTLCONT.replace(deleteDT, "").replace("3.", ">");
-            // const newtext = text.replace(/<[^>]*>?/g, '');
-            // moreInfo.innerHTML = newtext;
-            moreInfo.innerHTML = text;
-    
-            const longtitue = Number(v.X);
-            const lattitue = Number(v.Y);
-    
-            panTo(lattitue, longtitue);
-    
-            var marker = new kakao.maps.Marker({ 
-                // 지도 중심좌표에 마커를 생성합니다 
-                position: map.getCenter() 
-            });
-            marker.setMap(map);
+        // 이미지 변화
+        poster.setAttribute("src", v.IMGURL);
+        // title
+        infoTitle.innerHTML = v.SVCNM;
+
+        // SVCOPNBGNDT : 서비스 시작일시
+        // SVCOPNENDDT : 서비스 종료일시
+        // span1 : 날짜 -> string 변환후 substring으로 날짜만 표시.
+        span1.innerHTML = `${String(v.SVCOPNBGNDT).substring(0, 10)} ~ ${String(v.SVCOPNENDDT).substring(0, 10)}`;
+
+        // span2 : 결재방법
+        span2.innerHTML = v.PAYATNM;
+
+        // span3 : 서비스 상태
+        span3.innerHTML = v.SVCSTATNM;
+
+        // span4 : 지역명
+        span4.innerHTML = v.AREANM;
+
+
+        // moreInfo : 상세내용
+        const DTLCONTNum = v.DTLCONT.indexOf("3.");
+        const deleteDT = v.DTLCONT.slice(0, DTLCONTNum);
+        const text = v.DTLCONT.replace(deleteDT, "").replace("3.", ">");
+        // const newtext = text.replace(/<[^>]*>?/g, '');
+        // moreInfo.innerHTML = newtext;
+        moreInfo.innerHTML = text;
+
+        // 지도 이동
+        const longtitue = Number(v.X);
+        const lattitue = Number(v.Y);
+        
+        panTo(lattitue, longtitue);
+
+        // 마커가 표시될 위치입니다 
+        var markerPosition  = new kakao.maps.LatLng(lattitue, longtitue);
+
+        //이전 마커를 지웁니다
+        markers.forEach((v, i) => {
+            v.setMap(null);
+        });
+
+        // 마커를 생성합니다
+        var marker = new kakao.maps.Marker({
+            position: markerPosition
+        });
+
+        // 생성된 마커를 배열에 추가합니다
+        markers.push(marker);
+
+        // 마커가 지도 위에 표시되도록 설정합니다
+        marker.setMap(map);
     });
 
     document.querySelector('#delete').addEventListener('click' , (e) => {
--- js/map.js
+++ js/map.js
@@ -7,6 +7,9 @@ var mapContainer = document.getElementById('map'), // 지도를 표시할 div
 // 지도를 표시할 div와  지도 옵션으로  지도를 생성합니다
 var map = new kakao.maps.Map(mapContainer, mapOption);
 
+// 지도에 표시된 마커 객체를 가지고 있을 배열입니다
+var markers = [];
+
 function panTo(x, y) {
     // 이동할 위도 경도 위치를 생성합니다 
     var moveLatLon = new kakao.maps.LatLng(x, y);
